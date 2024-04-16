2bbd5872757ab5a9b9854df10695d0f41be3e1b7
Natalyasor/SN <natalyasor110983@gmail.com>
2024-04-16 21:52:49 +0200

    js-auth

--- src/component/field-select/index.js
+++ src/component/field-select/index.js
@@ -1,1 +1,41 @@
-window.fieldCheckbox = FieldCheckbox
+class FieldSelect {
+  static toggle = (target) => {
+    const options = target.nextElementSibling
+
+    options.toggleAttribute('active')
+
+    setTimeout(() => {
+      window.addEventListener(
+        'click',
+        (e) => {
+          alert(123)
+          if (!options.parentElement.contains(e.target))
+            options.removeAttribute('active')
+        },
+        { onse: true },
+      )
+    })
+  }
+
+  static change = (target) => {
+    const parent = target.parentElement.parentElement
+    const list = target.parentElement
+    const active =
+      target.parentElement.querySelector('*[active]')
+
+    if (active) active.toggleAttribute('active')
+
+    target.toggleAttribute('active')
+
+    const value = parent.querySelector('.field__value')
+
+    if (value) {
+      value.innerText = target.innerText
+      value.classList.remove('field__value--placeholder')
+    }
+
+    list.toggleAttribute('active')
+  }
+}
+
+window.fieldSelect = FieldSelect
