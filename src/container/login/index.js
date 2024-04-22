26d9fc3caa4dd1c12af6e10dcefe6534d774fbf2
Natalyasor/SN <natalyasor110983@gmail.com>
2024-04-22 09:22:21 +0200

    js-auth

--- src/container/login/index.js
+++ src/container/login/index.js
@@ -3,22 +3,18 @@ import {
   REG_EXP_EMAIL,
   REG_EXP_PASSWORD,
 } from '../../script/form'
+
 import { saveSession } from '../../script/session'
 
-class RecoveryConfirmForm extends Form {
+class SignupForm extends Form {
   FIELD_NAME = {
-    CODE: 'code',
+    EMAIL: 'email',
     PASSWORD: 'password',
-    PASSWORD_AGAIN: 'passwordAgain',
   }
   FIELD_ERROR = {
     iS_EMPTY: 'Введіть значення в поле',
     IS_BIG: 'Дуже довге значення, приберіть зайве',
-
-    PASSWORD:
-      'Пароль повинен складатися принаймі з 8 символів, включаючи хоча б одну цифру, малу та велику літеру',
-    PASSWORD_AGAIN:
-      'Ваш другий пароль не збігається з першим',
+    EMAIL: 'Введіть коректне значення e-mail адреси',
   }
 
   validate = (name, value) => {
@@ -29,19 +25,9 @@ class RecoveryConfirmForm extends Form {
     if (String(value).length > 20) {
       return this.FIELD_ERROR.IS_BIG
     }
-
-    if (name === this.FIELD_NAME.PASSWORD) {
-      if (!REG_EXP_PASSWORD.test(String(value))) {
-        return this.FIELD_ERROR.PASSWORD
-      }
-    }
-
-    if (name === this.FIELD_NAME.PASSWORD_AGAIN) {
-      if (
-        String(value) !==
-        this.value[this.FIELD_NAME.PASSWORD]
-      ) {
-        return this.FIELD_ERROR.PASSWORD_AGAIN
+    if (name === this.FIELD_NAME.EMAIL) {
+      if (!REG_EXP_EMAIL.test(String(value))) {
+        return this.FIELD_ERROR.EMAIL
       }
     }
   }
@@ -54,7 +40,7 @@ class RecoveryConfirmForm extends Form {
       this.setAlert('progress', 'Завантаження...')
 
       try {
-        const res = await fetch('/recovery-confirm', {
+        const res = await fetch('/login', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
@@ -79,13 +65,18 @@ class RecoveryConfirmForm extends Form {
 
   convertData = () => {
     return JSON.stringify({
-      [this.FIELD_NAME.CODE]: Number(
-        this.value[this.FIELD_NAME.CODE],
-      ),
+      [this.FIELD_NAME.EMAIL]:
+        this.value[this.FIELD_NAME.EMAIL],
       [this.FIELD_NAME.PASSWORD]:
         this.value[this.FIELD_NAME.PASSWORD],
     })
   }
 }
 
-window.recoveryConfirmForm = new RecoveryConfirmForm()
+window.signupForm = new SignupForm()
+
+document.addEventListener('DOMContentLoaded', () => {
+  if (window.session) {
+    location.assign('/')
+  }
+})
